cbe5142ff19f8d430b4d377530cb54dea49815fa
Gustavo Werle <gustavo.werle2@gmail.com>
2021-10-24 13:52:00 -0300

    test(TextField): configure helper to render with theme and add unit tests for TextField component

---
+++ src/components/TextInput/test.tsx
@@ -0,0 +1,111 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import TextField from '.'
+
+describe('<TextField />', () => {
+  it('Renders with Label', () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument()
+  })
+
+  it('Renders without Label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('Renders with placeholder', () => {
+    renderWithTheme(<TextField placeholder="unit testing" />)
+
+    expect(screen.getByPlaceholderText('unit testing')).toBeInTheDocument()
+  })
+
+  it('Changes its value when typing', async () => {
+    const onInput = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInput).toHaveBeenCalledWith(text)
+  })
+
+  it('Does not changes its value when disabled', async () => {
+    const onInput = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+    expect(onInput).not.toHaveBeenCalled()
+  })
+
+  it('Renders with error', () => {
+    const { container } = renderWithTheme(
+      <TextField label="TextField" labelFor="TextField" error="Error message" />
+    )
+
+    expect(screen.getByText('Error message')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Is accessible by tab', () => {
+    renderWithTheme(
+      <TextField label="TextField" labelFor="TextField" id="TextField" />
+    )
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).toHaveFocus()
+  })
+
+  it('Is not accessible by tab when disabled', () => {
+    renderWithTheme(
+      <TextField
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).not.toHaveFocus()
+  })
+})
